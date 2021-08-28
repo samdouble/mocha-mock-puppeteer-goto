fdbeb885eefe322c0cb4e6ca6e914281fe6d22a0
samdouble <samleafs@protonmail.com>
2021-08-27 23:32:01 -0400

    Version 0

---
+++ index.js
@@ -0,0 +1,30 @@
+const fs = require('fs');
+const sinon = require('sinon');
+
+module.exports = (config, page) => {
+  const stub = sinon.stub(page, 'goto');
+  stub.callsFake(async (url) => {
+    if (Array.isArray(config)) {
+      for (const configPage of config) {
+        if (url === configPage.url) {
+          const contents = fs.readFileSync(configPage.htmlPath);
+          await page.setContent(contents.toString());
+          return;
+        }
+      }
+      await page.setContent('');
+    } else if (typeof config === 'object' && config !== null) {
+      for (const [configUrl, configHtmlPage] of Object.entries(config)) {
+        if (url === configUrl) {
+          const contents = fs.readFileSync(configHtmlPage);
+          await page.setContent(contents.toString());
+          return;
+        }
+      }
+      await page.setContent('');
+    } else {
+      throw new Error('Error: Config must be an object or an array');
+    }
+  });
+  return stub;
+};
