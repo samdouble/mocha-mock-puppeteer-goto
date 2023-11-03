bcbba7e5cdeda5d57b5c0085d8d437153ad9921e
samdouble <samleafs@protonmail.com>
2023-11-02 21:44:18 -0400

    Convert to Typescript

---
+++ tests/myPuppeteerScript.ts
@@ -0,0 +1,29 @@
+interface Truck {
+  name: string;
+  engines: string[];
+}
+
+export default async function (page) {
+  await page.goto('https://somewebsite.com/');
+  const trucksPageUrlList = await page
+    .evaluate(async () => (
+      [...document.querySelectorAll('#trucks a')]
+        .map((truckLink) => truckLink.getAttribute('href'))
+    ));
+  const trucks: Truck[] = [];
+  for (const truckUrl of trucksPageUrlList) {
+    await page.goto(truckUrl);
+    const truck = await page
+      .evaluate(() => {
+        const name = [...document.querySelectorAll('h1')][0].innerText;
+        const engines = [...document.querySelectorAll('#engines ul li')]
+          .map((engineLi) => engineLi.innerHTML);
+        return {
+          name,
+          engines,
+        };
+      });
+    trucks.push(truck);
+  }
+  return trucks;
+};
